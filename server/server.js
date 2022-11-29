e2eed503dfd0bf36cea9e0f9fccacc144f8cd4bb
Lan Kostrikin <lankostrikin@gmail.com>
2022-11-28 19:08:05 -0600

    Final Version

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const SERVER_PORT = 4040;
+
+const inventory = ['gretting card', 'wagon', 'computer', 'table', 'chair', 'milk', 'sailboat', 'conditioner', 'rusty nail', 'desk']
+
+app.get('/api/inventory',(req,res) =>{
+    if(req.query.item){
+        const filteredItems = inventory.filter(invItem => invItem.toLowerCase().includes(req.query.item.toLowerCase()))
+        res.status(200).send(filteredItems)
+    } else{
+        res.status(200).send(inventory)
+    }
+    })
+    
+app.get(`/api/inventory/:index`, (req,res) =>{
+    res.status(200).send(inventory[+req.params.index])
+})
+
+app.listen(SERVER_PORT,() => console.log(`server running on ${SERVER_PORT}`));
